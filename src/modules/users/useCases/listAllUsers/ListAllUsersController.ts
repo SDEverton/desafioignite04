e548b7e0c648eaed5e1845e2d7a27b4ca66fee7e
Everton Oliveira <everton.belem@hotmail.com>
2021-03-21 14:17:34 -0300

    first commit

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,27 @@
 import { Request, Response } from "express";
+import { IncomingHttpHeaders } from "http";
 
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+
+interface IHeaders extends IncomingHttpHeaders {
+  user_id: string;
+}
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = <IHeaders>request.headers;
+    console.log(user_id);
+
+    try {
+      const users = this.listAllUsersUseCase.execute({ user_id });
+
+      console.log(users);
+      return response.json(users);
+    } catch (err) {
+      return response.status(400).json({ error: "mensagem do erro" });
+    }
   }
 }
 
